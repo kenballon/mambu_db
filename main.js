530afad992cb86ffc431edc85fbf169835e3b3bb
Ken Ballon <bkenn003@gmail.com>
2024-06-17 18:48:19 +0800

    upda

--- main.js
+++ main.js
@@ -36,8 +36,6 @@ formSubmit.addEventListener('click', e => {
     'username': username,
     'password': password,
   }
-  console.log(triggerDBObjects);
-  getUsers(username, password).then(() => console.log('User data fetched...')).catch(err => { console.error('error', err) });
 
   mambudb_backup(triggerDBObjects.webhookUrl, triggerDBObjects.tablesArr, triggerDBObjects.username, triggerDBObjects.password).then(() => console.log('Database backup triggered...')).catch(err => console.log('Error: ', err));
 })
